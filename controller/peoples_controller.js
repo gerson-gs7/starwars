2a4edb6b488ae2b5f0b1367645468758fa49fd70
F4476908 Iury Soprani Toledo Moreira <iury.moreira@bb.com.br>
2024-12-30 15:27:05 -0300

    atualização (restando paginação, bad requests e finalizar estilização)

--- controller/peoples_controller.js
+++ controller/peoples_controller.js
@@ -1,72 +1,134 @@
-import { peopleService } from "../service/peoples_service.js"
+import { peopleFetch } from "../service/peoples_service.js";
+import { homeworldFetch } from "../service/planets_service.js";
 
-const criaLinha = (personagem, planetaNatal) => {
-    const linhaNovoPersonagem = document.createElement('tr');
+//funçãod e criação de cards dos personagens
+const criaCard = (personagem) => {
+    const cardNovoPersonagem = document.createElement('div');
+    cardNovoPersonagem.classList.add('card', 'mb-3');
+    cardNovoPersonagem.dataset.name = personagem.name.toLowerCase();
+    cardNovoPersonagem.dataset.eyeColor = personagem.eye_color.toLowerCase();
+    cardNovoPersonagem.dataset.hairColor = personagem.hair_color.toLowerCase();
+    cardNovoPersonagem.dataset.gender = personagem.gender.toLowerCase();
+    cardNovoPersonagem.dataset.homeworld = typeof personagem.homeworldName === 'string' ? personagem.homeworldName.toLowerCase() : 'unknown';
     const conteudo = ` 
-        <td class="td" data-td>${substituirIndefinido(personagem.name)}</td>
-        <td>${substituirIndefinido(planetaNatal)}</td>
-        <td>${substituirIndefinido(personagem.eye_color)}</td>
-        <td>${substituirIndefinido(personagem.hair_color)}</td>
-        <td>${substituirIndefinido(personagem.gender)}</td>
-        <td>${substituirIndefinido(personagem.birth_year)}</td>
-    `;
-    linhaNovoPersonagem.innerHTML = conteudo;
-    return linhaNovoPersonagem;
+        <ul class="card-header">${substituirIndefinido(personagem.name)}</ul>
+        <div class="card-body">
+            <li class="card-text bg-dark text-white">Eye Color: ${substituirIndefinido(personagem.eye_color)}</li>
+            <li class="card-text">Hair Color: ${substituirIndefinido(personagem.hair_color)}</li>
+            <li class="card-text bg-dark text-white">Gender: ${substituirIndefinido(personagem.gender)}</li>
+            <li class="card-text">Birth Year: ${substituirIndefinido(personagem.birth_year)}</li>
+            <li class="card-text bg-dark text-white">Homeworld: ${substituirIndefinido(personagem.homeworldName)}</li>
+        </div>`;
+    cardNovoPersonagem.innerHTML = conteudo;
+    return cardNovoPersonagem;
 };
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const fetchTodosPersonagens = async () => {
-    let url = 'https://www.swapi.tech/api/people?page=1&limit=10';
-    let todosPersonagens = [];
-    let temProximaPagina = true;
-
-    while (temProximaPagina) {
-        
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            todosPersonagens = todosPersonagens.concat(data.results);
-            
-            if (data.next) {
-                url = data.next;
-                // console.log("teste");
-            } else {
-                temProximaPagina = false;
-            }
-        } catch (err) {
-            // console.error(err);
-            temProximaPagina = false;
-        }
-    }
-    // console.log(todosPersonagens);
-    
-    return todosPersonagens;
-};
-
+//substituindo o valor retornado de n/a para undefined
 const substituirIndefinido = (valor) => {
     return valor === "n/a" ? "undefined" : valor;
 };
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const tabela = document.querySelector('[data-tabela]');
+//controle de filtros e pesquisa
+const container = document.querySelector('[data-container]');
+const filtrarCards = () => {
+    const search = document.getElementById('searchInput').value.toLowerCase();
+    const eyeColor = document.getElementById('eyeColorInput').value.toLowerCase();
+    const hairColor = document.getElementById('hairColorInput').value.toLowerCase();
+    const gender = document.getElementById('genderSelectInput').value.toLowerCase();
+    
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        const name = card.dataset.name;
+        const cardEyeColor = card.dataset.eyeColor;
+        const cardHairColor = card.dataset.hairColor;
+        const cardGender = card.dataset.gender;
+        
+        const matchesSearch = !search || name.includes(search);
+        const matchesEyeColor = !eyeColor || cardEyeColor.includes(eyeColor);
+        const matchesHairColor = !hairColor || cardHairColor.includes(hairColor);
+        const matchesGender = !gender || cardGender === gender;
+        
+        if (matchesSearch && matchesEyeColor && matchesHairColor && matchesGender) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+};
 
+// Adicionar eventos de input para filtrar dinamicamente
+document.getElementById('searchInput').addEventListener('input', filtrarCards);
+document.getElementById('eyeColorInput').addEventListener('input', filtrarCards);
+document.getElementById('hairColorInput').addEventListener('input', filtrarCards);
+document.getElementById('genderSelectInput').addEventListener('change', filtrarCards);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//controle de paginação
+// let currentPage = 1;
+// const charactersPerPage = 10;
+
+// const renderPage = (personagens) => {
+//     container.innerHTML = ''; // Limpar os cards existentes
+//     const start = (currentPage - 1) * charactersPerPage;
+//     const end = start + charactersPerPage;
+//     const personagensPagina = personagens.slice(start, end);
+
+//     personagensPagina.forEach(personagem => {
+//         container.appendChild(criaCard(personagem));
+//     });
+
+//     updatePaginationControls(personagens.length);
+// };
+
+// const updatePaginationControls = (totalCharacters) => {
+//     const totalPages = Math.ceil(totalCharacters / charactersPerPage);
+//     const paginationContainer = document.querySelector('[data-pagination]');
+
+//     paginationContainer.innerHTML = ''; // Limpar os controles existentes
+
+//     for (let i = 1; i <= totalPages; i++) {
+//         const button = document.createElement('button');
+//         button.textContent = i;
+//         button.classList.add('pagination-button');
+//         if (i === currentPage) {
+//             button.classList.add('active');
+//         }
+//         button.addEventListener('click', () => {
+//             currentPage = i;
+//             renderPage(detalhesPersonagens);
+//         });
+//         paginationContainer.appendChild(button);
+//     }
+// };
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//método para renderizar os cards
 const render = async () => {
     try {
-        const personagens = await fetchTodosPersonagens();
+        const personagens = await peopleFetch();
         const detalhesPersonagens = await Promise.all(personagens.map(async (personagem) => {
             const response = await fetch(personagem.url);
             const data = await response.json();
             const personagemDetalhes = data.result.properties;
 
-            // Buscar o nome do planeta de origem
-            const planetaNatalResposta = await fetch(personagemDetalhes.homeworld);
-            const planetaNatalDados = await planetaNatalResposta.json();
-            const planetaNatal = planetaNatalDados.result.properties.name;
+            // Fetch homeworld name and save to cache
+            const homeworldName = await homeworldFetch(personagemDetalhes.homeworld);
+            personagemDetalhes.homeworldName = homeworldName;
 
-            return { ...personagemDetalhes, planetaNatal };
+            // console.log('Detalhes do personagem:', personagemDetalhes);
+
+            return { ...personagemDetalhes };
         }));
 
+        // renderPage(detalhesPersonagens); //Renderizar a primeira página
+
+        container.innerHTML = ''; // Limpar os cards existentes
         detalhesPersonagens.forEach(personagem => {
-            tabela.appendChild(criaLinha(personagem, personagem.planetaNatal));
+            container.appendChild(criaCard(personagem));
         });
+
     } catch (error) {
         console.log(error);
     }
--- service/peoples_service.js
+++ service/peoples_service.js
@@ -1,13 +1,32 @@
-const listaPersonagens = async () => {
-    try {
-        const response = await fetch(`https://www.swapi.tech/api/people`);
-        const data = await response.json();
-        return data.results;
-    } catch (err) {
-        console.error(err);
+export const peopleFetch = async () => {
+    const cacheKey = 'todosPersonagens';
+    const cachedData = localStorage.getItem(cacheKey);
+
+    if (cachedData) {
+        console.log('buscando personagem pelo cache');
+        console.log(localStorage.getItem('todosPersonagens'));
+        // console.log(cacheKey);
+        // console.log(cachedData);
+        return JSON.parse(cachedData);
+    } else {
+        console.log('buscando personagens pela api');
+
+        let url = 'https://www.swapi.tech/api/people?page=1&limit=82';
+        let todosPersonagens = [];
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            todosPersonagens = todosPersonagens.concat(data.results);
+            localStorage.setItem(cacheKey, JSON.stringify(todosPersonagens));
+
+        } catch (err) {
+            console.log('erro', err);
+        }
+        return todosPersonagens;
     }
 };
 
 export const peopleService = {
-    listaPersonagens
+    peopleFetch
 };
