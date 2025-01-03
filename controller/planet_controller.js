c68a789fa8d92e96fd21a43eb73e382b93b1f4e2
F3723486 Gerson Silva Costa <gersondobb@bb.com.br>
2025-01-03 15:31:40 -0300

    implementacao de busca pelo botao

--- controller/planet_controller.js
+++ controller/planet_controller.js
@@ -5,34 +5,76 @@ import { utils } from "../utils/utils.js";
 const data_container = document.querySelector('[data-container]');
 const pagination_menu = document.querySelector('[pagination]')
 const select = document.querySelector("select");
+const searchButton = document.getElementById('searchButton')
+
 let total_pages = ""
 var currentPage = 1
 let limit = 10
 
-const currentUrl = new URL(window.location.href);
-const params = new URLSearchParams(currentUrl.search);
-
 // Pegando os valores dos parâmetros da url
-if (params.get('page')) currentPage = params.get('page')
-if (params.get('limit')) limit = params.get('limit')
+const currentUrl = new URL(window.location.href);
+const paramsURL = new URLSearchParams(currentUrl.search);
+if (paramsURL.get('page')) currentPage = paramsURL.get('page')
+if (paramsURL.get('limit')) limit = paramsURL.get('limit')
 
-// Retorna uma array de planetas
-const planets_list = async (page, limit) => {
+// Pega o evento de click no Botão Search    
+searchButton.addEventListener('click',function(event){
+    event.preventDefault();
+    search()
+    
+})
+
+const search = async () => {
+
+    const searchInput = document.getElementById('searchInput');
+    
+    const params = `?name=${searchInput.value}`
+    render(params)
+}
+//Retorna uma array de planetas
+const planets_list_filter = async (params) => {
 
     const planets = []
-    const params = `?page=${page}&limit=${limit}`
+    
+    if (params === undefined) params = `?page=${currentPage}&limit=${limit}`
+    
     try {
         const dataAPI = await service.get_list("planets", params)
-        total_pages = dataAPI.total_pages
-        dataAPI.results.forEach(result => {
+        if(!dataAPI.result == []){
+            document.getElementById('loading-overlay').style.display = 'none';
+        }
+        dataAPI.result.forEach(result => {
             planets.push(result)
         })
-
+        
+        
         return planets
     } catch (error) {
         console.log(`Erro planets_list: ${error}`);
     }
 }
+// Retorna uma array de planetas
+const planets_list = async (params) => {
+
+    const planets = []
+ 
+    if (params === undefined)params = `?page=${currentPage}&limit=${limit}`
+
+    try {
+        const dataAPI = await service.get_list("planets", params)
+        
+        total_pages = dataAPI.total_pages
+        dataAPI.results.forEach(result => {
+            planets.push(result)
+        })
+        
+        
+        return planets
+    } catch (error) {
+        console.log(`Erro planets_list: ${error}`);
+    }
+}
+
 // Retorna uma instância do objeto Planet
 const get_planet = async (url) => {
     try {
@@ -56,16 +98,16 @@ const get_planet = async (url) => {
 const newCard = (planet) => {
     const model = `
                     <div class="card" style="width: 18rem;">
-                    <div class="card-header">
-                    <h5 class="card-title">${planet.get_name()}</h5>
-                    </div>
+                        <div class="card-header">
+                        <h5 class="card-title">${planet.get_name()}</h5>
+                        </div>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item bg-secondary">Rotation period: ${planet.get_rotation_period()}</li>
-                            <li class="list-group-item bg-ligth">Orbital period: ${planet.get_orbital_period()}</li>
-                            <li class="list-group-item bg-secondary">CLimate: ${planet.get_climate()}</li>
-                            <li class="list-group-item bg-ligth">Gravity: ${planet.get_gravity()}</li>
-                            <li class="list-group-item bg-secondary">Terrain: ${planet.get_terrain()}</li>
-                            <li class="list-group-item bg-ligth">Population: ${planet.get_population()}</li>
+                            <li class="list-group-item bg-dark text-white ps-1">&#8226 Rotation period: ${planet.get_rotation_period()}</li>
+                            <li class="list-group-item text-black ps-1">&#8226 Orbital period: ${planet.get_orbital_period()}</li>
+                            <li class="list-group-item bg-dark text-white ps-1">&#8226 CLimate: ${planet.get_climate()}</li>
+                            <li class="list-group-item text-black ps-1">&#8226 Gravity: ${planet.get_gravity()}</li>
+                            <li class="list-group-item bg-dark text-white ps-1">&#8226 Terrain: ${planet.get_terrain()}</li>
+                            <li class="list-group-item text-black ps-1">&#8226 Population: ${planet.get_population()}</li>
                         </ul>
                     </div>
     
@@ -74,9 +116,7 @@ const newCard = (planet) => {
     return card;
 }
 // Renderiza na tela todos os cards carregados
-const render = async (page, limit) => {
-    document.getElementById('loading-overlay').style.display = 'flex';
-    const list = await planets_list(page, limit)
+const render = async (params) => {
 
     while (data_container.firstChild) {
         data_container.removeChild(data_container.firstChild)
@@ -85,18 +125,37 @@ const render = async (page, limit) => {
         pagination_menu.removeChild(pagination_menu.firstChild)
     }
 
-    list.map(async (result) => {
+    document.getElementById('loading-overlay').style.display = 'flex';
+    
+    
+    
+    if (params !== undefined){
+        total_pages = 1;
+        const list = await planets_list_filter(params)
+        list.map(async (result) => {
 
-        const planet = await get_planet(result.url)
-        data_container.appendChild(newCard(planet));
+            const planet = await get_planet(result.properties.url)
+            data_container.appendChild(newCard(planet));
+    
+            document.getElementById('loading-overlay').style.display = 'none';
+            rolarParaElemento()
+    
+        })
+    }else{
+        const list = await planets_list()
+        list.map(async (result) => {
 
-        document.getElementById('loading-overlay').style.display = 'none';
-        rolarParaElemento()
-
-    })
-    pagination_menu.appendChild(utils.createPagination(page, limit, total_pages));
+            const planet = await get_planet(result.url)
+            data_container.appendChild(newCard(planet));
+    
+            document.getElementById('loading-overlay').style.display = 'none';
+            rolarParaElemento()    
+        })        
+    }    
+    pagination_menu.appendChild(utils.createPagination(currentPage, limit, total_pages));
 }
-//render(currentPage, limit)
+
+render()
 
 // Pega o evento de troca de opção no select
 select.addEventListener("change", function (event) {
@@ -107,7 +166,7 @@ select.addEventListener("change", function (event) {
 
 const optionToSelect = select.querySelector(`option[value="${limit}"]`);
 optionToSelect.selected = true;
-
+//SCROLLAR
 function rolarParaElemento() {
     const elemento = document.getElementById("container");
     window.scrollTo({
